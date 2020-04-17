8743346a3b4a09fa98318d978d571f3fe05d746e
KRN <krnbde5@gmail.com>
2020-04-18 05:04:31 +0530

    Added Map
    Steps:
    1. follow all necessary steps included in the official documentation https://developers.google.com/maps/documentation/android-sdk/start
    2. Register the API, and don't forget to enable that specific API type.. follow this link for more details https://bit.ly/2KbcnqD
    3. Declare the api key in the Manifest using <meta-data
    4. That's all, code written here is the most basic format to include mapView in a fragment/activity(modify a bit). This code should include onStart(), onPause(), etc methods..  I will do it in the next commit.

--- app/src/main/java/com/krnventures/covid_19/ui/infectedLocations/InfectedLocationsViewModel.kt
+++ app/src/main/java/com/krnventures/covid_19/ui/infectedLocations/InfectedLocationsViewModel.kt
@@ -1,14 +1,8 @@
 package com.krnventures.covid_19.ui.infectedLocations
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.krnventures.covid_19.dtos.essentialsArrayListDTO
-import com.krnventures.covid_19.network.ApiComponent
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class InfectedLocationsViewModel : ViewModel() {
 
